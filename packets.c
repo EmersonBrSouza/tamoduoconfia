14f96d4d742041cfaa635d068171a7623997c360
EmersonBrSouza <emerson_souza_fsa@hotmail.com>
2018-11-25 21:44:37 -0300

    Packets

---
+++ packets.c
@@ -0,0 +1,18 @@
+#define WIFI_MODE "AT+CWMODE=3"
+#define WIFI_CONNECT "AT+CWJAP=\"SSID\", \"PASSWORD\""
+
+#define TCP_CONNECT "AT+CIPSTART=\"TCP\",\"IP\",PORT"
+#define TCP_DISCONNECT "AT+CIPCLOSE"
+
+#define MQTT_CONNECT_SIZE "AT+CIPSEND=20"
+#define MQTT_CONNECT "\x10\x12\x00\x04\aMQTT\x04\x02\x00\x14\x00\x06\aNios 2"
+
+#define MESSAGE_SIZE "AT+CIPSEND=21"
+#define MESSAGE_0 "\x30\x13\x00\x06\aPBL 02Mensagem 01"
+#define MESSAGE_1 "\x30\x13\x00\x06\aPBL 02Mensagem 02"
+#define MESSAGE_2 "\x30\x13\x00\x06\aPBL 02Mensagem 03"
+#define MESSAGE_3 "\x30\x13\x00\x06\aPBL 02Mensagem 04"
+#define MESSAGE_4 "\x30\x13\x00\x06\aPBL 02Mensagem 05"
+
+#define MQTT_DISCONNECT_SIZE "AT+CIPSEND=2"
+#define MQTT_DISCONNECT "\xe0\x00"
